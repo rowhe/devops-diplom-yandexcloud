7aed31b6436e3e9ed335e878b8f8807d09280f0a
Denis <95048755+rowhe@users.noreply.github.com>
2023-03-24 11:45:08 +0300

    playing whith service account

--- diploma/terraform/main.tf
+++ diploma/terraform/main.tf
@@ -125,28 +125,29 @@ resource "yandex_vpc_subnet" "subnet10_2" {
   network_id     = "${yandex_vpc_network.diploma_network.id}"
 }
 
-//// Create SA
-//resource "yandex_iam_service_account" "sa" {
-//  folder_id = "${var.yc_folder_id}"
-//  name      = "tf-test-sa"
-//}
-//
-//// Grant permissions
-//resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
-//  folder_id = "${var.yc_folder_id}"
+// Create SA
+resource "yandex_iam_service_account" "sa" {
+  folder_id = "${var.yc_folder_id}"
+  name      = "tf-test-sa"
+}
+
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
+  folder_id = "${var.yc_folder_id}"
+  role      = "admin"
 //  role      = "storage.editor"
-//  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
-//}
-//
-//// Create Static Access Keys
-//resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-//  service_account_id = yandex_iam_service_account.sa.id
-//  description        = "static access key for object storage"
-//}
-//
-//// Use keys to create bucket
-//resource "yandex_storage_bucket" "diploma-bucket" {
-//  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-//  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-//  bucket = "bigbucket"
-//}
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "diploma-bucket" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = "bigbucket"
+}
