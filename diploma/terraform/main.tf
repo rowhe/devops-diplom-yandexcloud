52853fd1c9237b03530ff7fc4b88783933915a4f
Denis <95048755+rowhe@users.noreply.github.com>
2023-04-27 12:38:13 +0300

    s3 backend added to terraform main.tf

--- diploma/terraform/main.tf
+++ diploma/terraform/main.tf
@@ -4,6 +4,16 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "diploma-tf-bucket"
+    region     = "ru-central1"
+    key        = "terraform/main.tfstate"
+    token = "var.yc_token"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
   required_version = ">= 0.13"
 }
 
